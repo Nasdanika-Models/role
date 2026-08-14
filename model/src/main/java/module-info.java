1b9938d379607d99503b769dc55af0f6b71b7f63
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2026-08-14 07:38:13 -0400

    Dependency cleanup, copying xcore model to the site root

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -7,8 +7,6 @@ module org.nasdanika.models.role {
 	exports org.nasdanika.models.role.impl;
 	exports org.nasdanika.models.role.util;
 	
-	requires transitive org.eclipse.emf.ecore;
-	requires transitive org.eclipse.emf.common;
 	requires transitive org.nasdanika.models.nxcore;	
 	
 	provides CapabilityFactory with 
